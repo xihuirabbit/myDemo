3cbf92252649420c6a73c3f0f29ac053f826c1e4
lihan <751778380@qq.com>
2019-09-28 01:23:53 +0800

    更新

--- demo.js
+++ demo.js
@@ -1,8 +1,15 @@
 (function () {
+    const myObj = document.getElementById("ifrPage100002");
     // 获取cookie备用
     const aa = document.cookie;
     let cookiesTotal= aa.split(";");
     let cookiesData = cookiesTotal[1].split("=");
     let cookies = cookiesData[1];
     console.log(cookies);
+
+    //输入框存储
+    let inputEvent = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
+    inputEvent.onchange = function () {
+        console.log(inputEvent.value)
+    }
 })();
