1349dde218a427c82f4eeecd3f91bf91ec9df3a8
lihan <751778380@qq.com>
2019-09-28 02:07:33 +0800

    更新

--- demo.js
+++ demo.js
@@ -1,6 +1,4 @@
 (function () {
-    var myObj = document.getElementById("ifrPage100002");
-    console.log(myObj)
     // 获取cookie备用
     const aa = document.cookie;
     let cookiesTotal= aa.split(";");
@@ -8,21 +6,12 @@
     let cookies = cookiesData[1];
     console.log(cookies);
 
+
+    let myObj = document.getElementById("ifrPage100002");
+    console.log(myObj);
+
     //输入框存储
-    let dataInput = document.getElementById("ifrPage100002").contentWindow.document.querySelector("#qryCustomerInputStr");
-    console.log(dataInput);
-    // let dataInput1 = "";
-    // dataInput.onchange = function () {
-    //     console.log(inputEvent.value)
-    //     sessionStorage.setItem('dataInput', dataInput.value);
-    //     dataInput1 = sessionStorage.getItem("dataInput");
-    //     console.log(dataInput1);
-    // }
+    let inputObj  = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
+    console.log(inputObj)
 
-    //搜索项存储
-    // let searchEvent = myObj.contentWindow.document.querySelector("#multSearchType").getElementsByTagName("span");
-    // console.log(searchEvent);
-    // let searchData = searchEvent[0]
-
-    // 点击事件选择
 })();
