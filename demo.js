9068975b18e5b09c5f0b3497e6a31893176cfe1e
lihan <751778380@qq.com>
2019-09-29 00:26:53 +0800

    更新

--- demo.js
+++ demo.js
@@ -12,6 +12,8 @@
 
     let inputdata = "";
     let searchData = "accNbr";
+    let listObj = "";
+
     let seacchBty = myObj.contentWindow.document.getElementsByClassName("yn-search-ico")[0];
     console.log(seacchBty)
     if(seacchBty){
@@ -30,12 +32,56 @@
             searchData = searchObj[0].getAttribute("data-type");
             console.log(searchData);
 
-            // 点击事件
-            let listObj = myObj.contentWindow.document.querySelector("#resultList");
+            // 点击获取接口数据
+            listObj = myObj.contentWindow.document.querySelector("#resultList");
             console.log(listObj);
-
-
+        }
+    };
+    listObj.onclick = function () {
+        let chooseObj = myObj.contentWindow.document.querySelector("#mainContent");
+        console.log(chooseObj);
+        chooseObj.onclick = function () {
+            setTimeout(function (){
+              let answerObj = myObj.contentWindow.document.querySelector("#content1");
+              console.log(answerObj)
+              let answerdatas = answerObj.getElementsByTagName("span")[0].innerHTML;
+              console.log(answerdatas);
+              let num = answerdatas.split("");
+              let answerData = `${num[num.length-4]}${num[num.length-3]}${num[num.length-2]}${num[num.length-1]}`;
+              console.log(answerData);
+                getData(answerData);
+            }, 5000);
         }
     }
+    function getData(answerData) {
+        let params1 = `[${inputdata},${searchData},${cookies},"3c156b221d634851a79b446ad23246ec"]`;
+        console.log(params1)
+        let options = {
+            "widgetName":"searchOffer",
+            "methodName":"searchCustForFixedAccNum",
+            "params":params1,
+            "selector":"#searchList",
+            "keyName":""
+        };
+        console.log(options);
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = callback1;
+        xhr.open("post", "http://crm3.yn.189.cn:9500/crm/so/refreshPart",true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(options);
 
-});
+        function callback1() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                let data = xhr.responseText;//获取数据
+                console(data);
+                let dataCard = "";
+                //card 比对
+                const card = myObj.contentWindow.document.querySelector("#certNumSuffix");
+                if(card != null){
+                    card.value = dataCard;
+                    dataCard.removeAttribute('disabled');
+                }
+            }
+        }
+    }
+})();
