fbc2e72e8e06fe029d4584857acb8a88b08c9c4a
lihan <751778380@qq.com>
2019-09-28 13:32:26 +0800

    更新

--- demo.js
+++ demo.js
@@ -35,4 +35,45 @@
             console.log(searchData);
         }
     }
+
+    // 新增按钮
+    let divObj = myObj.contentWindow.document.querySelector("#readCertBtn");
+    console.log(divObj);
+    let divBty = myObj.contentWindow.document.createElement("input");
+    divBty.setAttribute("type", "button");
+    divBty.setAttribute("value", "数据");
+    divBty.setAttribute("id", "divBty");
+    divBty.style.width = "15px";
+    divBty.setAttribute("onclick", "getData()");
+    divObj.appendChild(divBty);
+     function getData() {
+         let params1 = `[${inputdata},${searchData},${cookies},"3c156b221d634851a79b446ad23246ec"]`;
+         console.log(params1)
+         let options = {
+             "widgetName":"searchOffer",
+             "methodName":"searchCustForFixedAccNum",
+             "params":params1,
+             "selector":"#searchList",
+             "keyName":""
+         };
+         console.log(options);
+         const xhr = new XMLHttpRequest();
+         xhr.onreadystatechange = callback1;
+         xhr.open("post", "http://crm3.yn.189.cn:9500/crm/so/refreshPart",true);
+         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+         xhr.send(options);
+
+         function callback1() {
+             if (xhr.readyState == 4 && xhr.status == 200) {
+                 let data = xhr.responseText;//获取数据
+                 console(data);
+                 let dataCard = "";
+                 const card = myObj.contentWindow.document.querySelector("#certNumSuffix");
+                 if(card != null){
+                     card.value = dataCard;
+                     dataCard.removeAttribute('disabled');
+                 }
+             }
+         }
+     }
 })();
