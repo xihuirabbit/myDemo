880fb1cc5295d7d8e51faed9b382bb0fa354fbdc
lihan <751778380@qq.com>
2019-09-30 00:45:15 +0800

    更新

--- demo.js
+++ demo.js
@@ -17,48 +17,56 @@
         if(seacchBty){
             alert("点击确定请开始你的表演");
             seacchBty.onclick = function () {
-                let inputObj  = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
-                if(inputObj){
-                    inputdata = inputObj.value;
-                }
-
-                let searchObj = myObj.contentWindow.document.querySelector("#multSearchType").getElementsByTagName("span");
-                if( searchObj[0].getAttribute("data-type")){
-                    searchData = searchObj[0].getAttribute("data-type");
-                }
-
-                setTimeout(function () {
-                    let listObj = myObj.contentWindow.document.querySelector("#resultList");
-                    if(listObj){
-                        alert("劝告该系统开发人员一句，别再坑爹了，转行吧！！！！");
-                    }
-                    listObj.addEventListener("click", function(e){
-                        // e.stopPropagation();
-                        alert("点击确定下一步");
-                        setTimeout(function (){
-                            // 存储搜索页
-                            let pageObj = myObj.contentWindow.document.getElementsByClassName("active")[0];
-                            if(pageObj){
-                                page = pageObj.innerHTML;
-                            }
-
-                            let answerObj = myObj.contentWindow.document.querySelector("#content1");
-                            let answerdatas = answerObj.getElementsByTagName("span")[0].innerHTML;
-                            let num = answerdatas.split("");
-                             answerData = `${num[num.length-4]}${num[num.length-3]}${num[num.length-2]}${num[num.length-1]}`;
-                            getData();
-                        }, 3000);
-                    });
-                },2000)
-
-
+                jumpPage();
             }
         };
-
         // 自动弹框
         let downBty = myObj.contentWindow.document.querySelector("#multSearchType").getElementsByTagName("button");
         console.log(downBty);
+        if(downBty){
+            downBty.onclick = function () {
+               setTimeout(function () {
+                   alert("傻逼开发的锅，点击确定下一步");
+                   jumpPage()
+               },4000)
 
+            }
+        };
+        function jumpPage() {
+            let inputObj  = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
+            if(inputObj){
+                inputdata = inputObj.value;
+            }
+
+            let searchObj = myObj.contentWindow.document.querySelector("#multSearchType").getElementsByTagName("span");
+            if( searchObj[0].getAttribute("data-type")){
+                searchData = searchObj[0].getAttribute("data-type");
+            }
+            setTimeout(function () {
+                let listObj = myObj.contentWindow.document.querySelector("#resultList");
+                if(listObj){
+                    alert("劝告该系统开发人员一句，别再坑爹了，转行吧！！！！");
+                }
+                listObj.addEventListener("click", function(e){
+                    // e.stopPropagation();
+                    alert("点击确定下一步");
+                    setTimeout(function (){
+                        // 存储搜索页
+                        let pageObj = myObj.contentWindow.document.getElementsByClassName("active")[0];
+                        if(pageObj){
+                            page = pageObj.innerHTML;
+                        }
+
+                        let answerObj = myObj.contentWindow.document.querySelector("#content1");
+                        let answerdatas = answerObj.getElementsByTagName("span")[0].innerHTML;
+                        let num = answerdatas.split("");
+                        answerData = `${num[num.length-4]}${num[num.length-3]}${num[num.length-2]}${num[num.length-1]}`;
+                        getData();
+                    }, 3000);
+                });
+            },2000);
+
+        }
         function getData() {
             let params1 = "";
             let methodName = "";
