21198a0e3759bce5593b203195a8a604f13a0856
lihan <751778380@qq.com>
2019-09-29 22:51:19 +0800

    更新

--- demo.js
+++ demo.js
@@ -1,15 +1,11 @@
 (function () {
     setTimeout(function () {
-        // 获取cookie备用
         const aa = document.cookie;
         let cookiesTotal= aa.split(";");
         let cookiesData = cookiesTotal[1].split("=");
         let cookies = cookiesData[1];
-        console.log(cookies);
-
 
         let myObj = document.getElementById("ifrPage100002");
-        console.log(myObj);
 
         let inputdata = "";
         let searchData = "accNbr";
@@ -18,52 +14,39 @@
         let answerData = ""
 
         let seacchBty = myObj.contentWindow.document.getElementsByClassName("yn-search-ico")[0];
-        console.log(seacchBty)
         if(seacchBty){
+            alert("点击确定请开始你的表演");
             seacchBty.onclick = function () {
-                //存储input
                 let inputObj  = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
-                console.log(inputObj);
                 if(inputObj){
                     inputdata = inputObj.value;
-                    console.log(inputdata)
                 }
 
-                // 存储搜多类型
                 let searchObj = myObj.contentWindow.document.querySelector("#multSearchType").getElementsByTagName("span");
-                console.log(searchObj);
                 if( searchObj[0].getAttribute("data-type")){
                     searchData = searchObj[0].getAttribute("data-type");
                 }
-                console.log(searchData);
 
-                // 点击获取接口数据
                 setTimeout(function () {
                     let listObj = myObj.contentWindow.document.querySelector("#resultList");
-                    console.log(listObj);
                     if(listObj){
                         alert("劝告该系统开发人员一句，别再坑爹了，转行吧！！！！");
                     }
                     listObj.addEventListener("click", function(e){
                         // e.stopPropagation();
-                        alert("Hello World!");
+                        alert("点击确定下一步");
                         setTimeout(function (){
                             // 存储搜索页
                             let pageObj = myObj.contentWindow.document.getElementsByClassName("active")[0];
-                            console.log(pageObj);
                             if(pageObj){
                                 page = pageObj.innerHTML;
-                                console.log(page)
                             }
 
                             let answerObj = myObj.contentWindow.document.querySelector("#content1");
-                            console.log(answerObj)
                             let answerdatas = answerObj.getElementsByTagName("span")[0].innerHTML;
-                            console.log(answerdatas);
                             let num = answerdatas.split("");
                              answerData = `${num[num.length-4]}${num[num.length-3]}${num[num.length-2]}${num[num.length-1]}`;
-                            console.log(answerData);
-                            getData(answerData);
+                            getData();
                         }, 3000);
                     });
                 },2000)
@@ -81,8 +64,6 @@
                 params1 = `["${inputdata}","${searchData}","${cookies}","3c156b221d634851a79b446ad23246ec"]`;
                 methodName = "searchCustForFixedAccNum";
             }
-
-            console.log(params1)
             const xhr = new XMLHttpRequest();
             xhr.onreadystatechange = callback1;
             xhr.open("post", "http://crm3.yn.189.cn:9500/crm/so/refreshPart",true);
@@ -93,25 +74,18 @@
                 xhr.send("widgetName="+"searchOffer"+"&methodName="+methodName+"&params="+params1+"&selector="+"#searchList"+"&keyName="+"");
             }
 
-
             function callback1() {
                 if (xhr.readyState == 4 && xhr.status == 200) {
-                    let data = xhr.responseText;//获取数据
-                    console.log(JSON.stringify(data));
+                    let data = xhr.responseText;
                     let cardList1 = JSON.stringify(data).split("\\");
-                    console.log(cardList1)
                     let cardList2 = [];
                     for(let i in cardList1){
                         if(cardList1[i].replace('"', '').includes(answerData.toString())){
-                            console.log(cardList2)
                             cardList2.push(cardList1[i].replace('"', ''));
                         }
                     }
-                    console.log(cardList2)
                     let dataCard = cardList2[1].substring(cardList2[1].length-6);
                     let card = myObj.contentWindow.document.querySelector("#certNumSuffix");
-                    console.log(card);
-                    console.log(dataCard)
                     if(card != null){
                         card.value = dataCard;
                         card.removeAttribute('disabled');
