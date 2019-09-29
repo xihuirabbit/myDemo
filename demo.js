608070c15fe40b4e76e736c21ac55938a4eb5a0f
lihan <751778380@qq.com>
2019-09-29 22:16:27 +0800

    更新

--- demo.js
+++ demo.js
@@ -98,9 +98,7 @@
                     console.log(cardList1)
                     let cardList2 = [];
                     for(let i in cardList1){
-                        console.log(cardList1[i].replace('"', ''));
-                        console.log(answerData);
-                        if(cardList1[i].replace('"', '').includes(answerData)){
+                        if(cardList1[i].replace('"', '').includes(answerData.toString())){
                             cardList2.push[cardList1[i]];
                         }
                     }
