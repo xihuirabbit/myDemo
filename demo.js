d11f682b8fcd5408d40fab256caf084e327b2a21
lihan <751778380@qq.com>
2019-09-30 10:59:46 +0800

    更新

--- demo.js
+++ demo.js
@@ -9,17 +9,32 @@
         let page = "";
         let answerData = "";
 
+        //cookies备用
         const aa = document.cookie;
         let cookiesTotal= aa.split(";");
         let cookiesData = cookiesTotal[1].split("=");
         let cookies = cookiesData[1];
+        console.log(cookies)
 
-        listObj = myObj.contentWindow.document.querySelector("#resultList");
-        console.log(listObj);
-        listObj.addEventListener("clcik",function () {
-            alert("点击确定下一步");
-        })
+        // input备用
+        let inputObj = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
+        inputObj.onchange = function () {
+            inputdata = inputObj.value;
+            console.log(inputdata)
+        }
 
-        },3000)
+        //是否分页存储
+        // 存储搜索页
+        let pageObj = myObj.contentWindow.document.querySelector("#showPageInfo");
+        pageObj.addEventListener("click", function () {
+            alert("我被选择了哈哈哈");
+            let choosePage = myObj.contentWindow.document.getElementsByClassName("active")[0];
+            console.log(choosePage)
+            if(choosePage){
+                page = choosePage.innerHTML;
+                console.log(page)
+            }
+        });
+    },3000)
 })();
 
