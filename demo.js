f95a9e102bd022730f36995dda14baf8c0a90bd6
lihan <751778380@qq.com>
2019-10-12 09:54:19 +0800

    更新

--- demo.js
+++ demo.js
@@ -5,24 +5,25 @@
     let cookiesData = cookiesTotal[1].split("=");
     let cookies = cookiesData[1];
     console.log(cookies)
-    getIframe();
+    // getIframe();
+    function getIframe() {
+        let myObj = document.getElementById("ifrPage100002");
+        if(myObj){
+            console.log(myObj);
+            getfirstModal ();
+        }else{
+            getIframe ();
+        }
+    };
 
 })();
-function getIframe() {
-    let myObj = document.getElementById("ifrPage100002");
-    if(myObj){
-        console.log(myObj);
-        getfirstModal ();
-    }else{
-        getIframe ();
-    }
-};
-function getfirstModal(){
-    let firstModal = document.getElementById("ifrPage100002").contentWindow.document.querySelector("#searchList");
-    if(firstModal){
-        console.log(firstModal)
-    }else{
-        getfirstModal ();
-    }
-}
+
+// function getfirstModal(){
+//     let firstModal = document.getElementById("ifrPage100002").contentWindow.document.querySelector("#searchList");
+//     if(firstModal){
+//         console.log(firstModal)
+//     }else{
+//         getfirstModal ();
+//     }
+// }
 
