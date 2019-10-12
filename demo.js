59df3d9f66f02b10c5d0b563f862a73db357ed5b
lihan <751778380@qq.com>
2019-10-12 11:44:58 +0800

    更新

--- demo.js
+++ demo.js
@@ -1,22 +1,23 @@
 (function () {
     let myObj = "";
-    //cookies备用
-    const aa = document.cookie;
-    let cookiesTotal= aa.split(";");
-    let cookiesData = cookiesTotal[1].split("=");
-    let cookies = cookiesData[1];
-    console.log(cookies);
-
+    let cookies = "";
     getIframe();
     function getIframe() {
        myObj = document.getElementById("ifrPage100002");
         if(myObj){
             console.log(myObj);
+            //cookies备用
+            const aa = document.cookie;
+            let cookiesTotal= aa.split(";");
+            let cookiesData = cookiesTotal[1].split("=");
+            cookies = cookiesData[1];
+            console.log(cookies);
             getfirstModal ();
         }else{
             getIframe ();
         }
-    };
+    }
+
     function getfirstModal() {
         let firstModal = myObj.contentWindow.document.querySelector("#searchList");
         if(firstModal){
