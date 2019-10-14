a87c45b3ef2e6c428cffe253de7e38271a6a95d5
lihan <751778380@qq.com>
2019-10-14 17:37:33 +0800

    更新

--- demo.js
+++ demo.js
@@ -2,6 +2,13 @@
     let myObj = "";
     let cookies = "";
     let time = "";
+    // 变量
+    let inputdata = "";
+    let searchData = "accNbr";
+    let listObj = "";
+    let page = "";
+    let answerData = "";
+    let pageLength = "";
     getIframe();
     function getIframe() {
        myObj = document.getElementById("ifrPage100002");
@@ -23,9 +30,35 @@
         setTimeout(function () {
             let firstModal = myObj.contentWindow.document.querySelector("#searchList");
             if(firstModal){
-                console.log(firstModal)
+                console.log(firstModal);
+                firstModal.addEventListener('click', function(e) {
+                    // input 输入框；
+                    let inputObj = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
+                    console.log(inputObj)
+                    inputdata = inputObj.value;
+                    console.log(inputdata)
+
+                    // 类型
+                    let downBty = myObj.contentWindow.document.querySelector("#multSearchType");
+                    console.log(downBty);
+                    let searchObj = downBty.getElementsByTagName("span");
+                    console.log(searchObj)
+                    if( searchObj[0].getAttribute("data-type")){
+                        searchData = searchObj[0].getAttribute("data-type");
+                    }
+                    console.log(searchData)
+
+                    //存储搜索页
+                    pageLength = myObj.contentWindow.document.querySelector("#showPageInfo").getElementsByTagName("a");
+                    console.log(pageLength.length);
+                    let pageObj = myObj.contentWindow.document.getElementsByClassName("active")[0];
+                    if(pageObj){
+                        page = pageObj.innerHTML;
+                    }
+                    console.log(page);
+
+                })
             }else{
-                console.log("nbb");
                 getfirstModal ();
             }
         },100)
