17994b0d525b2f19a830584122272590c06b4b22
lihan <751778380@qq.com>
2019-10-16 17:36:54 +0800

    更新

--- demo.js
+++ demo.js
@@ -13,7 +13,6 @@
     function getIframe() {
        myObj = document.getElementById("ifrPage100002");
         if(myObj){
-            console.log(myObj);
             //cookies备用
             const aa = document.cookie;
             let cookiesTotal= aa.split(";");
@@ -24,7 +23,6 @@
                 }
             })
             cookies = cookiesData[1];
-            console.log(cookies);
             getfirstModal ();
         }else{
             getIframe ();
@@ -35,38 +33,30 @@
         setTimeout(function () {
             let firstModal = myObj.contentWindow.document.querySelector("#searchList");
             if(firstModal){
-                console.log(firstModal);
                 firstModal.addEventListener('click', function(e) {
                     // input 输入框；
                     let inputObj = myObj.contentWindow.document.querySelector("#qryCustomerInputStr");
-                    console.log(inputObj)
                     inputdata = inputObj.value;
-                    console.log(inputdata);
 
 
                     // 类型
                     let reg = /^((0\d{2,3}-\d{7,8})|(1[34578]\d{9}))$/;
                     if ( !reg.test(inputdata)){
                         let downBty = myObj.contentWindow.document.querySelector("#multSearchType");
-                        console.log(downBty);
                         let searchObj = downBty.getElementsByTagName("span");
-                        console.log(searchObj)
                         if( searchObj[0].getAttribute("data-type")){
                             searchData = searchObj[0].getAttribute("data-type");
                         }
                     }else{
                         searchData = "accNbr"
                     }
-                    console.log(searchData)
 
                     //存储搜索页
                     pageLength = myObj.contentWindow.document.querySelector("#showPageInfo").getElementsByTagName("a");
-                    console.log(pageLength.length);
                     let pageObj = myObj.contentWindow.document.getElementsByClassName("active")[0];
                     if(pageObj){
                         page = pageObj.innerHTML;
                     }
-                    console.log(page);
 
                     getSecondModal();
                 })
@@ -80,16 +70,13 @@
         setTimeout(function () {
             let answerObj = myObj.contentWindow.document.querySelector("#content1");
             if(answerObj){
-                console.log(answerObj);
                 let answerdatas = answerObj.getElementsByTagName("span")[0].innerHTML;
                 let num = answerdatas.split("");
                 answerData = `${num[num.length-4]}${num[num.length-3]}${num[num.length-2]}${num[num.length-1]}`;
                 getData();
 
                 let nextReadObj = myObj.contentWindow.document.querySelector("#readCertBtn");
-                console.log(nextReadObj)
                 nextReadObj.addEventListener('click', function(e) {
-                    console.log("bbb");
                     getThirdModal();
                 });
 
@@ -169,7 +156,7 @@
             let imgObj = myObj.contentWindow.document.querySelector("#IdPhoto");
             console.log(imgObj);
             imgObj.onclick = function () {
-
+                alert(上传功能待开发)
             };
             if(readBty){
 
